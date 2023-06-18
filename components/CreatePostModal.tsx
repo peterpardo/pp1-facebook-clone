ed7fe6d286433b810322baeddc1c43aded1a8ef1
Peter Carl Pardo <peterpardo123@gmail.com>
2023-06-18 19:28:26 +0800

    Fixed create post modal

--- components/CreatePostModal.tsx
+++ components/CreatePostModal.tsx
@@ -3,73 +3,91 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
-import { FaCaretDown, FaUserFriends } from "react-icons/fa";
+import { FaCaretDown, FaTimes, FaUserFriends } from "react-icons/fa";
 
-const CreatePostModal = () => {
+type CreatePostModalType = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const CreatePostModal = ({ isOpen, onClose }: CreatePostModalType) => {
   const { data: session } = useSession();
   const [input, setInput] = useState("");
 
+  const handleClose = () => {
+    setInput("");
+    onClose();
+  };
+
   return (
-    <div className="fixed inset-0 z-20">
-      {/* Backdrop */}
-      <div
-        className="fixed inset-0 bg-gray-100 opacity-50"
-        onClick={() => console.log("Backdrop")}
-      ></div>
+    isOpen && (
+      <div className="fixed inset-0 z-20">
+        {/* Backdrop */}
+        <div
+          className="fixed inset-0 bg-gray-100 opacity-50"
+          onClick={handleClose}
+        />
 
-      {/* Modal */}
-      <div className="flex items-center justify-center h-full">
-        <div className="bg-white rounded-md drop-shadow-md min-w-[500px]">
-          {/* title */}
-          <div className="text-center font-bold border-b-2 py-4">
-            Create Post
-          </div>
-
-          {/* User with info */}
-          <div className="flex items-center gap-x-2 mx-4 py-3">
-            <div className="w-10 rounded-full cursor-pointer overflow-hidden">
-              <Image
-                src={session?.user.image as string}
-                alt="user image"
-                width={40}
-                height={40}
-                className="w-full"
-              />
-            </div>
-            <div className="text-xs">
-              <div className="font-semibold">{session?.user.name}</div>
-              <div className="flex items-center gap-x-2 bg-gray-200 rounded-md p-1 mt-1 cursor-pointer">
-                <FaUserFriends />
-                <span className="font-semibold">Friends</span>
-                <FaCaretDown />
+        {/* Modal */}
+        <div className="flex items-center justify-center h-full">
+          <div className="bg-white rounded-md drop-shadow-md min-w-[500px]">
+            {/* title */}
+            <div className="relative text-center font-bold border-b-2 py-5">
+              <div>Create Post</div>
+              <div
+                className="text-gray-500 absolute right-3 top-2/4 -translate-y-1/2 bg-gray-200 rounded-full p-3 cursor-pointer hover:bg-gray-300"
+                onClick={handleClose}
+              >
+                <FaTimes />
               </div>
             </div>
-          </div>
 
-          {/* textarea */}
-          <textarea
-            className="w-full px-4 outline-none h-36 resize-none"
-            placeholder={`What's on your mind, ${session?.user.name}?`}
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
+            {/* User with info */}
+            <div className="flex items-center gap-x-2 mx-4 py-3">
+              <div className="w-10 rounded-full cursor-pointer overflow-hidden">
+                <Image
+                  src={session?.user.image as string}
+                  alt="user image"
+                  width={40}
+                  height={40}
+                  className="w-full"
+                />
+              </div>
+              <div className="text-xs">
+                <div className="font-semibold">{session?.user.name}</div>
+                <div className="flex items-center gap-x-2 bg-gray-200 rounded-md p-1 mt-1 cursor-pointer">
+                  <FaUserFriends />
+                  <span className="font-semibold">Friends</span>
+                  <FaCaretDown />
+                </div>
+              </div>
+            </div>
 
-          {/* button */}
-          <div className="px-4 py-3">
-            <button
-              className={`${
-                input
-                  ? "bg-[color:var(--fb-blue)] text-white"
-                  : "bg-gray-200 text-gray-300"
-              } font-bold text-center rounded-md py-2 w-full`}
-              disabled={!input}
-            >
-              Post
-            </button>
+            {/* textarea */}
+            <textarea
+              className="w-full px-4 outline-none h-36 resize-none"
+              placeholder={`What's on your mind, ${session?.user.name}?`}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+
+            {/* button */}
+            <div className="px-4 py-3">
+              <button
+                className={`${
+                  input
+                    ? "bg-[color:var(--fb-blue)] text-white"
+                    : "bg-gray-200 text-gray-300"
+                } font-bold text-center rounded-md py-2 w-full`}
+                disabled={!input}
+              >
+                Post
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 export default CreatePostModal;
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useSession } from "next-auth/react";
 import Image from "next/image";
@@ -10,8 +10,8 @@ const Feed = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
 
   const handleShowModal = () => {
-    setIsOpenModal(true)
-  }
+    setIsOpenModal(true);
+  };
 
   return (
     <section className="flex flex-col justify-center mx-auto max-w-[680px] w-full p-2 gap-y-3">
@@ -29,18 +29,26 @@ const Feed = () => {
             />
           </div>
           {/* text */}
-          <div className="flex items-center rounded-3xl bg-gray-100 py-1 px-4 flex-1 cursor-pointer hover:bg-gray-200" onClick={handleShowModal}>
+          <div
+            className="flex items-center rounded-3xl bg-gray-100 py-1 px-4 flex-1 cursor-pointer hover:bg-gray-200"
+            onClick={handleShowModal}
+          >
             <span className="text-gray-500">{`What's on your mind, ${session?.user.name}?`}</span>
           </div>
         </div>
 
-        <hr className="hidden md:block"/>
+        <hr className="hidden md:block" />
 
         <div className="justify-around items-center gap-x-2 hidden md:flex">
           <div className="text-sm flex items-center justify-center rounded-md hover:bg-gray-100 gap-x-2 cursor-pointer px-5 py-1 flex-1">
             <div className="w-5 md:w-10">
               {/* image */}
-              <Image src='/icons/video-player.png' width={30} height={30} alt='video icon'/>
+              <Image
+                src="/icons/video-player.png"
+                width={30}
+                height={30}
+                alt="video icon"
+              />
             </div>
             <div>
               <span>Live Video</span>
@@ -49,7 +57,12 @@ const Feed = () => {
           <div className="text-sm flex items-center justify-center rounded-md hover:bg-gray-100 gap-x-2 cursor-pointer px-5 py-1 flex-1">
             <div className="w-5 md:w-10">
               {/* image */}
-              <Image src='/icons/picture.png' width={30} height={30} alt='video icon'/>
+              <Image
+                src="/icons/picture.png"
+                width={30}
+                height={30}
+                alt="video icon"
+              />
             </div>
             <div>
               <span>Photo/video</span>
@@ -58,7 +71,12 @@ const Feed = () => {
           <div className="text-sm flex items-center justify-center rounded-md hover:bg-gray-100 gap-x-2 cursor-pointer px-5 py-1 flex-1">
             <div className="w-5 md:w-10">
               {/* image */}
-              <Image src='/icons/happy.png' width={30} height={30} alt='video icon'/>
+              <Image
+                src="/icons/happy.png"
+                width={30}
+                height={30}
+                alt="video icon"
+              />
             </div>
             <div>
               <span>Feeling/activity</span>
@@ -69,7 +87,10 @@ const Feed = () => {
 
       {/* List of Posts */}
 
-      <CreatePostModal/>
+      <CreatePostModal
+        isOpen={isOpenModal}
+        onClose={() => setIsOpenModal(false)}
+      />
     </section>
   );
 };
